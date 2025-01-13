e211ff475a909a373cad6b4f4ad096f02ef3a627
rkarnati <ravi@kalpatechllc.com>
2025-01-12 23:16:18 -0600

    initial commit

---
+++ src/app/catering/catering.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-catering',
+  imports: [],
+  templateUrl: './catering.component.html',
+  styleUrl: './catering.component.css'
+})
+export class CateringComponent {
+
+}
