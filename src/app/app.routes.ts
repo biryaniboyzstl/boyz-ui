0df0b90fd81aa083ac84e03f8b6798072e7fe340
rkarnati <ravi@kalpatechllc.com>
2025-01-13 13:20:08 -0600

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
+import { provideRouter } from '@angular/router';
+import { importProvidersFrom } from '@angular/core';
 
-export const routes: Routes = [];
+// Import the standalone components
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { CateringComponent } from './catering/catering.component';
+import { ContactComponent } from './contact/contact.component';
+import { MenuComponent } from './menu/menu.component';
+import { OrderComponent } from './order/order.component';
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'catering', component: CateringComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'menu', component: MenuComponent },
+  { path: 'order', component: OrderComponent },
+  { path: '**', redirectTo: 'home' }, // Wildcard route for unknown paths
+];
+
+// Provide routing in the application
+export const AppRoutingProviders = [
+  provideRouter(routes),
+];
---
+++ src/app/order-online/order-online.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-order-online',
+  imports: [],
+  templateUrl: './order-online.component.html',
+  styleUrl: './order-online.component.css'
+})
+export class OrderOnlineComponent {
+
+}
