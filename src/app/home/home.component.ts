67429459c628b22afd9f731cfb1a45c26226bc16
rkarnati <ravi@kalpatechllc.com>
2025-03-29 11:39:53 -0500

    weekend buffet

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,5 @@
-
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 
 @Component({
   selector: 'app-home',
@@ -11,13 +9,15 @@ import { CommonModule } from '@angular/common';
   styleUrl: './home.component.css'
 })
 export class HomeComponent implements OnInit {
-   buffetPrice!: number;
-
+  buffetPrice!: number;
+  kidsPrice!: number;
 
   ngOnInit(): void {
     const today = new Date();
     const day = today.getDay(); // 0 = Sunday, 6 = Saturday
     // Weekends (Saturday=6, Sunday=0) use $20.99; Weekdays use $15.99
     this.buffetPrice = (day === 0 || day === 6) ? 20.99 : 15.99;
+    // Kids (12 and under) pay half of the adult price.
+    this.kidsPrice = parseFloat((this.buffetPrice / 2).toFixed(2));
   }
 }
